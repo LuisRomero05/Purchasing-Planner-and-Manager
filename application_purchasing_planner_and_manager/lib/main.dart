38b443bb2ec2110065344962b08da55915ae9881
Luis Angel Romero <romeroluisangel735@gmail.com>
2024-08-20 01:29:06 -0600

    Conexion a la base de Datos
    Conexion a la base de Datos, pendiente la verificacion

--- application_purchasing_planner_and_manager/lib/main.dart
+++ application_purchasing_planner_and_manager/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:application_purchasing_planner_and_manager/pages/inicio.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Asegúrate de inicializar WidgetsFlutterBinding
+  await Firebase.initializeApp(); // Inicializa Firebase
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
