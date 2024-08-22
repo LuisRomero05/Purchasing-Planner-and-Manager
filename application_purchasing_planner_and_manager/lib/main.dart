5a725d960764f63d0e7331bf7c4fac794a952508
Luis Angel Romero <romeroluisangel735@gmail.com>
2024-08-22 00:39:23 -0600

    Revert "Conexion a la base de Datos"
    This reverts commit 38b443bb2ec2110065344962b08da55915ae9881.

--- application_purchasing_planner_and_manager/lib/main.dart
+++ application_purchasing_planner_and_manager/lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:application_purchasing_planner_and_manager/pages/inicio.dart';
 import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Asegúrate de inicializar WidgetsFlutterBinding
-  await Firebase.initializeApp(); // Inicializa Firebase
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
